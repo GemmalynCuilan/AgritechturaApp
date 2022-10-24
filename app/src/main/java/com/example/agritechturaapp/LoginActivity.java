267873068e877ec4306cf5fd18e3611584fff070
Your Name <you@example.com>
2022-10-23 20:32:35 -0800

    Agritechtura_App(update)

--- app/src/main/java/com/example/agritechturaapp/LoginActivity.java
+++ app/src/main/java/com/example/agritechturaapp/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.agritechturaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView username =(TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+
+
     }
 }
