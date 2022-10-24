e1bf6129ac26f5c561c9fc0bd978c0cf9b62b9d3
Your Name <you@example.com>
2022-10-23 22:28:21 -0800

    Agritechtura_App(update)

--- app/src/main/java/com/example/agritechturaapp/LoginActivity.java
+++ app/src/main/java/com/example/agritechturaapp/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.agritechturaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -13,11 +15,14 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextView username =(TextView) findViewById(R.id.username);
-        TextView password = (TextView) findViewById(R.id.password);
 
-        Button loginButton = (Button) findViewById(R.id.loginButton);
-
+        TextView backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/agritechturaapp/SignUpActivity.java
+++ app/src/main/java/com/example/agritechturaapp/SignUpActivity.java
@@ -2,7 +2,10 @@ package com.example.agritechturaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        TextView backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
